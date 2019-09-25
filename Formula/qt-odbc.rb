99355078489d0fd5ff1195fd520efa57f8779c70
gsong <gsong2014@foxmail.com>
2019-09-26 04:49:50 +0700

    add qt packages and wx*

---
+++ Formula/qt-odbc.rb
@@ -0,0 +1,126 @@
+class QtOdbc < Formula
+  desc "Qt SQL Database Driver - QODBC for Open Database Connectivity (ODBC)"
+  homepage "https://doc.qt.io/qt-5/sql-driver.html"
+  url "https://download.qt.io/official_releases/qt/5.12/5.12.3/single/qt-everywhere-src-5.12.3.tar.xz"
+  sha256 "6462ac74c00ff466487d8ef8d0922971aa5b1d5b33c0753308ec9d57711f5a42"
+
+  # revision 1
+
+  head "https://code.qt.io/qt/qt5.git", :branch => "5.12", :shallow => false
+
+  bottle do
+    root_url "https://bottle.download.osgeo.org"
+    cellar :any
+    sha256 "8a8624df5f2ddd32b6c5d3707383674841b07b6a2622ed4d093f43dae278f89b" => :mojave
+    sha256 "8a8624df5f2ddd32b6c5d3707383674841b07b6a2622ed4d093f43dae278f89b" => :high_sierra
+    sha256 "b4a7cc14916f3b01b9f18493d607f0927e768b56aac7f119fde2efbd9837798b" => :sierra
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "libiconv"
+  depends_on "gettext"
+  depends_on "libxml2"
+  depends_on "libxslt"
+  depends_on "zlib"
+  depends_on "qt"
+  depends_on "sqlite"
+  depends_on "openssl"
+  depends_on "unixodbc"
+  depends_on :xcode => :build
+
+  def install
+    qt_plugins = lib/"qt/plugins"
+    (qt_plugins/"sqldrivers").mkpath
+
+    chdir "#{buildpath}/qtbase/src/plugins/sqldrivers" do
+      system "#{Formula["qt"].opt_bin}/qmake", "--", "ODBC_PREFIX=#{Formula["unixodbc"].opt_prefix}"
+      system "make"
+
+      # copy libqsqlodbc.dylib
+      # libqsqlite.dylib from qt
+      cp_r "#{buildpath}/qtbase/src/plugins/sqldrivers/plugins/sqldrivers/libqsqlodbc.dylib", "#{lib}/qt/plugins/sqldrivers/"
+    end
+  end
+
+  def caveats; <<~EOS
+    Plugins generated are linked to the following directory:
+
+      #{HOMEBREW_PREFIX}/lib/qt/plugins/sqldrivers
+
+    EOS
+  end
+
+  test do
+    (testpath/"hello.pro").write <<~EOS
+      QT        += core sql
+      QT        -= gui
+      TARGET     = hello
+      CONFIG    += console
+      CONFIG    -= app_bundle
+      TEMPLATE   = app
+      HEADERS    = client.h
+      SOURCES   += client.cpp main.cpp
+    EOS
+
+    (testpath/"client.h").write <<~EOS
+    #ifndef CLIENT_H
+    #define CLIENT_H
+    #include <QObject>
+    #include <QSqlDatabase>
+    #include <QString>
+    class Client : public QObject
+    {
+      Q_OBJECT
+    public:
+      Client(QObject *parent = 0);
+    public slots:
+      void checkSqlDriver();
+    };
+    #endif // CLIENT_H
+    EOS
+
+    (testpath/"client.cpp").write <<~EOS
+    #include "client.h"
+    #include <QCoreApplication>
+    #include <QDebug>
+    #include <QSqlDatabase>
+    #include <QStringList>
+    Client::Client(QObject *parent)
+      : QObject(parent)
+    {
+    }
+    void Client::checkSqlDriver()
+    {
+      QCoreApplication::addLibraryPath("#{HOMEBREW_PREFIX}/lib/qt/plugins");
+      QCoreApplication::addLibraryPath("#{lib}/qt/plugins");
+      qDebug() << "QSqlDatabase::drivers(): " << QSqlDatabase::drivers().join(" ");
+      QCoreApplication::exit(!QSqlDatabase::isDriverAvailable("QODBC"));
+    }
+    EOS
+
+    (testpath/"main.cpp").write <<~EOS
+      #include <QCoreApplication>
+      #include <QDebug>
+      #include <QTimer>
+      #include "client.h"
+      int main(int argc, char *argv[])
+      {
+        QCoreApplication app(argc, argv);
+        Client c(app.instance());
+        qDebug() << "Running application";
+        QTimer::singleShot(1000, &c, SLOT(checkSqlDriver()));
+        return app.exec();
+      }
+    EOS
+
+    cd testpath do
+      system Formula["qt"].bin/"qmake", "hello.pro"
+      system "make"
+      assert_predicate testpath/"client.o", :exist?
+      assert_predicate testpath/"moc_client.o", :exist?
+      assert_predicate testpath/"main.o", :exist?
+      assert_predicate testpath/"hello", :exist?
+      system "./hello"
+    end
+  end
+end
---
+++ Formula/qtkeychain.rb
@@ -0,0 +1,82 @@
+class Unlinked < Requirement
+  fatal true
+
+  satisfy(:build_env => false) { !core_qtkeychain_linked }
+
+  def core_qtkeychain_linked
+    Formula["qtkeychain"].linked_keg.exist?
+  rescue
+    return false
+  end
+
+  def message
+    s = "\033[31mYou have other linked versions!\e[0m\n\n"
+
+    s += "Unlink with \e[32mbrew unlink qtkeychain\e[0m or remove with brew \e[32muninstall --ignore-dependencies qtkeychain\e[0m\n\n" if core_qtkeychain_linked
+    s
+  end
+end
+
+class Qtkeychain < Formula
+  desc "Platform-independent Qt-based API for storing passwords securely"
+  homepage "https://github.com/frankosterfeld/qtkeychain"
+  url "https://github.com/frankosterfeld/qtkeychain/archive/v0.9.1.tar.gz"
+  sha256 "9c2762d9d0759a65cdb80106d547db83c6e9fdea66f1973c6e9014f867c6f28e"
+
+  revision 2
+
+  head "https://github.com/frankosterfeld/qtkeychain.git", :using => :git
+
+  option "with-static", "Build static in addition to shared library"
+  option "with-translations", "Generate Qt translation (.ts) files"
+
+  # keg_only "qtkeychain" is already provided by homebrew/core"
+  # we will verify that other versions are not linked
+  depends_on Unlinked
+
+  depends_on "cmake" => :build
+  depends_on "qt"
+
+  def lib_name
+    "libqt5keychain"
+  end
+
+  def install
+    args = std_cmake_args
+    args << "-DQTKEYCHAIN_STATIC=OFF"
+    args << "-DBUILD_WITH_QT4=OFF"
+    args << "-DBUILD_TRANSLATIONS=#{build.with?("translations") ? "ON" : "OFF"}"
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+      (libexec/"bin").install "testclient"
+      so_ver = 1
+      lib_name_ver = "#{lib_name}.#{so_ver}"
+      MachO::Tools.change_install_name("#{libexec}/bin/testclient",
+                                       "@rpath/#{lib_name_ver}.dylib",
+                                       "#{opt_lib}/#{lib_name_ver}.dylib")
+
+      if build.with? "static"
+        args << "-DQTKEYCHAIN_STATIC=ON"
+        system "cmake", "..", *args
+        system "make"
+        mv "#{lib_name}.a", lib/"#{lib_name}_static.a"
+      end
+    end
+  end
+
+  def caveats
+    if build.with? "static"
+      <<~EOS
+        Static library is available at:
+          #{opt_lib}/#{lib_name}_static.a
+      EOS
+    end
+  end
+
+  test do
+    assert_match "Password deleted successfully",
+                 shell_output(libexec/"bin/testclient delete something-particular")
+  end
+end
